c4381008702992e006da19038b95736608685b82
agrojas <rojasagustin90@gmail.com>
2015-09-23 22:33:35 -0300

    Login facebook navegando a pantalla principal

--- App/app/src/main/java/com/support/android/designlibdemo/LoginActivity.java
+++ App/app/src/main/java/com/support/android/designlibdemo/LoginActivity.java
@@ -18,192 +18,116 @@ import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.facebook.share.Sharer;
 import com.facebook.share.widget.ShareDialog;
 
 public class LoginActivity extends FragmentActivity {
-    private CallbackManager callbackManager;
-    private ProfileTracker profileTracker;
+
+
     private ShareDialog shareDialog;
-    private PendingAction pendingAction = PendingAction.NONE;
-    private static final String PERMISSION = "publish_actions";
-    private final String PENDING_ACTION_BUNDLE_KEY =
-            "com.example.hellofacebook:PendingAction";
-    private FacebookCallback<Sharer.Result> shareCallback = new FacebookCallback<Sharer.Result>() {
-        @Override
-        public void onCancel() {
-            Log.d("HelloFacebook", "Canceled");
-        }
+    private CallbackManager callbackManager;
+    private AccessTokenTracker accessTokenTracker;
+    private ProfileTracker profileTracker;
 
+    private FacebookCallback<LoginResult> callback = new FacebookCallback<LoginResult>() {
         @Override
-        public void onError(FacebookException error) {
-            Log.d("HelloFacebook", String.format("Error: %s", error.toString()));
-            String title = getString(R.string.error);
-            String alertMessage = error.getMessage();
-            showResult(title, alertMessage);
-        }
-
-        @Override
-        public void onSuccess(Sharer.Result result) {
+        public void onSuccess(LoginResult loginResult) {
+            AccessToken accessToken = loginResult.getAccessToken();
+            Profile profile = Profile.getCurrentProfile();
+           //showResult(profile.getFirstName(), profile.getLastName());
             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
             startActivity(intent);
-            Log.d("HelloFacebook", "Success!");
-            if (result.getPostId() != null) {
-                String title = getString(R.string.success);
-                String id = result.getPostId();
-                String alertMessage = getString(R.string.successfully_posted_post, id);
-                showResult(title, alertMessage);
-            }
         }
 
-        private void showResult(String title, String alertMessage) {
-            new AlertDialog.Builder(LoginActivity.this)
-                    .setTitle(title)
-                    .setMessage(alertMessage)
-                    .setPositiveButton(R.string.ok, null)
-                    .show();
+        @Override
+        public void onCancel() {
+
         }
+
+        @Override
+        public void onError(FacebookException e) {
+
+        }
+
     };
 
 
-    private enum PendingAction {
-        NONE,
-        POST_PHOTO,
-        POST_STATUS_UPDATE
+    private void showResult(String title, String alertMessage) {
+        new AlertDialog.Builder(LoginActivity.this)
+                .setTitle(title)
+                .setMessage(alertMessage)
+                .setPositiveButton(R.string.ok, null)
+                .show();
     }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
         FacebookSdk.sdkInitialize(this.getApplicationContext());
 
+
         callbackManager = CallbackManager.Factory.create();
+
+        accessTokenTracker= new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldToken, AccessToken newToken) {
+
+            }
+        };
+
+        profileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile newProfile) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                //showResult(newProfile.getFirstName(),newProfile.getLastName());
+            }
+        };
+
+        accessTokenTracker.startTracking();
+        profileTracker.startTracking();
+
         setContentView(R.layout.activity_login);
         final Button button = (Button) findViewById(R.id.btn_login);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
             }
         });
-        LoginManager.getInstance().registerCallback(callbackManager,
-                new FacebookCallback<LoginResult>() {
-                    @Override
-                    public void onSuccess(LoginResult loginResult) {
-                        handlePendingAction();
-                        updateUI();
-                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                        startActivity(intent);
-                    }
-
-                    @Override
-                    public void onCancel() {
-                        if (pendingAction != PendingAction.NONE) {
-                            showAlert();
-                            pendingAction = PendingAction.NONE;
-                        }
-                        updateUI();
-                    }
-
-                    @Override
-                    public void onError(FacebookException exception) {
-                        if (pendingAction != PendingAction.NONE
-                                && exception instanceof FacebookAuthorizationException) {
-                            showAlert();
-                            pendingAction = PendingAction.NONE;
-                        }
-                        updateUI();
-                    }
-
-                    private void showAlert() {
-                        new AlertDialog.Builder(LoginActivity.this)
-                                .setTitle(R.string.cancelled)
-                                .setMessage(R.string.permission_not_granted)
-                                .setPositiveButton(R.string.ok, null)
-                                .show();
-                    }
-                });
-
-
-        shareDialog = new ShareDialog(this);
-        shareDialog.registerCallback(
-                callbackManager,
-                shareCallback);
-
-        if (savedInstanceState != null) {
-            String name = savedInstanceState.getString(PENDING_ACTION_BUNDLE_KEY);
-            pendingAction = PendingAction.valueOf(name);
-        }
-
-
-        profileTracker = new ProfileTracker() {
-            @Override
-            protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
-                updateUI();
-                // It's possible that we were waiting for Profile to be populated in order to
-                // post a status update.
-                handlePendingAction();
-            }
-        };
 
 
 
 
-    }
-
+    };
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onStop() {
+        super.onStop();
+        accessTokenTracker.stopTracking();
+        profileTracker.stopTracking();
     }
 
-    private void updateUI() {
-        boolean enableButtons = AccessToken.getCurrentAccessToken() != null;
+    @Override
+    public void onResume() {
+        super.onResume();
         Profile profile = Profile.getCurrentProfile();
+        if(profile != null){
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            //showResult(profile.getFirstName(),profile.getLastName());
+        }
 
-//        if (enableButtons && profile != null) {
-//            profilePictureView.setProfileId(profile.getId());
-//            greeting.setText(getString(R.string.hello_user, profile.getFirstName()));
-//        } else {
-//            profilePictureView.setProfileId(null);
-//            greeting.setText(null);
-//        }
     }
 
-    private void handlePendingAction() {
-        PendingAction previouslyPendingAction = pendingAction;
-        // These actions may re-set pendingAction if they are still pending, but we assume they
-        // will succeed.
-//        pendingAction = PendingAction.NONE;
-//
-//        switch (previouslyPendingAction) {
-//            case NONE:
-//                break;
-//            case POST_PHOTO:
-//                //postPhoto();
-//                break;
-//            case POST_STATUS_UPDATE:
-//                //postStatusUpdate();
-//                break;
-//        }
-    }
+
 }
