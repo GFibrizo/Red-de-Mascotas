42f7623f11f76e93130a9f43074cb0cdfc4722ab
jasmina.sella <jasmina.sella@fdvsolutions.com>
2015-09-26 15:41:03 -0300

    Agrego Pantalla de Resultado de Busqueda (por ahora con acceso por menu asi la puedo ver, despues la linkeo a la pantalla de busqueda)

---
+++ App/app/src/main/java/com/support/android/designlibdemo/ResultListActivity.java
@@ -0,0 +1,73 @@
+package com.support.android.designlibdemo;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class ResultListActivity extends AppCompatActivity {
+
+    private TabLayout mTabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_list);
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_result);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null)
+            actionBar.setDisplayHomeAsUpEnabled(true);
+
+
+        /*mTabLayout = (TabLayout) findViewById(R.id.tabs);
+
+        Tab newTab = mTabLayout.newTab();
+        newTab.setText("Publicar en adopción");
+        mTabLayout.addTab(newTab);
+        mTabLayout.setSelected(false);
+        mTabLayout.setTabMode(TabLayout.MODE_FIXED);
+        mTabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        mTabLayout.invalidate();*/
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_result_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
