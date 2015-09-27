b56405810ebba2d8442dd05a09dfcb02732ce88d
jasmina.sella <jasmina.sella@fdvsolutions.com>
2015-09-27 18:47:10 -0300

    Agrego paquete "model" con clases MascotaAdopcion y Domicilio para ser usadas en Android

---
+++ App/app/src/main/java/com/support/android/designlibdemo/model/Domicilio.java
@@ -0,0 +1,59 @@
+package com.support.android.designlibdemo.model;
+
+public class Domicilio {
+
+    public String calle;
+    public String numero;
+    public String barrio;
+    public String ciudad;
+    public String provincia;
+    public String pais;
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getBarrio() {
+        return barrio;
+    }
+
+    public void setBarrio(String barrio) {
+        this.barrio = barrio;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+}
