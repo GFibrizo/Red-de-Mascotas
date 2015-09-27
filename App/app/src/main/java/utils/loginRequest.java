9400f56a18b2ef0b9d8211b63cc9c2236f0acb35
agrojas <rojasagustin90@gmail.com>
2015-09-27 16:22:59 -0300

    conexion con el server sincronica

--- App/app/src/main/java/utils/loginRequest.java
+++ App/app/src/main/java/utils/loginRequest.java
@@ -21,80 +21,49 @@ import java.util.concurrent.TimeoutException;
  * Created by agu_k_000 on 27/09/2015.
  */
 public class LoginRequest {
-    private Context actualContext;
     private String salt;
-
+    RequestHandler requestHandler;
     public LoginRequest(Context context) {
-        this.actualContext = context;
+        requestHandler = RequestHandler.getInstance(context);
         this.salt= "";
     }
 
 
     //Sincronico
-//    public String getUserSalt(String user) {
-//        RequestHandler requestHandler = RequestHandler.getInstance(this.actualContext);
-//        String url = "/usuario/" + user + "/salt";
-//        RequestFuture<String> future = RequestFuture.newFuture();
-//        StringRequest request = new StringRequest(Request.Method.GET, RequestHandler.getServerUrl() + url,future,future);
-//        String salt = null; // this line will block
-//        try {
-//            salt = future.get();
-//        } catch (InterruptedException e) {
-//            // exception handling
-//        } catch (ExecutionException e) {
-//            // exception handling
-//        }
-//        // exception handling
-//        return salt;
-//    }
-
-    //Asincronico
     public String getUserSalt(String user) {
-        RequestHandler requestHandler = RequestHandler.getInstance(this.actualContext);
-        String path = "/usuario/" + user + "/salt";
-        //final String[] salt = {null};
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, requestHandler.getServerUrl() + path,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        //complete salt
-                        salt = response;
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
 
-            }
-        });
+        requestHandler.setServerUrl("http://192.168.1.106:9000");
+        String url = "/usuario/" + user + "/salt";
+        RequestFuture<String> future = RequestFuture.newFuture();
+        StringRequest request = new StringRequest(Request.Method.GET, RequestHandler.getServerUrl() + url,future,future);
+        requestHandler.addToRequestQueue(request);
+        String salt = null; // this line will block
+        try {
+            salt = future.get();
+        } catch (InterruptedException e) {
+            // exception handling
+        } catch (ExecutionException e) {
+            // exception handling
+        }
         // exception handling
         return salt;
     }
 
     public boolean isValidUserPassword(String user, Password password) {
-        RequestHandler requestHandler = RequestHandler.getInstance(this.actualContext);
-        String url = "/login/cuenta";
-        // Mapeo de los pares clave-valor
-        HashMap<String, String> parameters = new HashMap();
-        parameters.put("nombreDeUsuario", user);
-        parameters.put("contraseniaEncriptada", password.getEncriptacion());
-
-        JsonObjectRequest request = new JsonObjectRequest(
-                Request.Method.GET,
-                RequestHandler.getServerUrl() + url,
-                new JSONObject(parameters),
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject jsonResponse) {
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-
-                    }
-                }
-        );
+        String url = "/login/cuenta?nombreDeUsuario=" + user + "&contraseniaEncriptada=" + password.getEncriptacion();
+        RequestFuture<JSONObject> future = RequestFuture.newFuture();
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, RequestHandler.getServerUrl() + url, future, future);
         requestHandler.addToRequestQueue(request);
+        JSONObject response = null;
+        try {
+            response = future.get();
+        } catch (InterruptedException e) {
+            // exception handling
+            return false;
+        } catch (ExecutionException e) {
+            // exception handling
+            return false;
+        }
         return true;
     }
 }
