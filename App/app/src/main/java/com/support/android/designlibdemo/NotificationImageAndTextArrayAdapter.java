a13af8bfee42e25f363feec3af68f98b042a728e
jasmina.sella <jasmina.sella@fdvsolutions.com>
2015-10-05 02:59:18 -0300

    Agrego listView a las notificaciones

---
+++ App/app/src/main/java/com/support/android/designlibdemo/NotificationImageAndTextArrayAdapter.java
@@ -0,0 +1,63 @@
+package com.support.android.designlibdemo;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.support.android.designlibdemo.data.communications.ImageUrlView;
+
+import java.util.ArrayList;
+
+/**
+ * Clase que servira para mostrar en un listView algo que tenga un texto y una imagen
+ *
+ */
+public class NotificationImageAndTextArrayAdapter extends ArrayAdapter<TextAndImage> {
+    protected final Activity context;
+    protected final ArrayList<TextAndImage> elements;
+    protected final int layout;
+    protected String baseUrlForImage;
+    private String IP_EMULADOR = "http://10.0.2.2:9000"; //ip generica del emulador
+    /**
+     * @param layout   El layout que usara para mostrar cada fila
+     * @param elements conjunto de elementos en los cuales se mostrara 1 por fila
+     */
+    public NotificationImageAndTextArrayAdapter(Activity context, int layout, String baseUrlForImage, ArrayList<TextAndImage> elements) {
+        super(context, layout, elements);
+        this.layout = layout;
+        this.context = context;
+        this.elements = elements;
+        this.baseUrlForImage = baseUrlForImage;
+    }
+
+    public View getView(final int position, final View view, final ViewGroup parent) {
+//        if (elements.size() == 0) {
+//            return new View(getContext());
+//        }
+//        final TextAndImage element = elements.get(position);
+
+        final LayoutInflater inflater = context.getLayoutInflater();
+        View rowView = inflater.inflate(this.layout, null, true);
+
+        TextView fecha = (TextView) rowView.findViewById(R.id.fechaAdoptar);
+        TextView quiereAdoptar = (TextView) rowView.findViewById(R.id.quiereAdoptar);
+        TextView contacto = (TextView) rowView.findViewById(R.id.contactoAdoptar);
+
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.rowimage);
+
+        fecha.setText(fecha.getText());
+        quiereAdoptar.setText(quiereAdoptar.getText());
+        contacto.setText(contacto.getText());
+
+
+//        int id = element.getId(); //String.valueOf(id)
+        int id = 2;
+        baseUrlForImage = IP_EMULADOR + "/pet/image/" + String.valueOf(id);
+        new ImageUrlView(baseUrlForImage, imageView).connect();
+        return rowView;
+    }
+}
