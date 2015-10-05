e4648363dca1f781737169077b4d2db5caede2fd
GFibrizo <zebas.graffe@gmail.com>
2015-10-04 21:29:31 -0300

    Agregadas dependencias

---
+++ App/app/src/main/java/ColorPickerDialog.java
@@ -0,0 +1,5 @@
+/**
+ * Created by fabrizio on 03/10/15.
+ */
+public class ColorPickerDialog {
+}
---
+++ App/app/src/main/java/utils/UploadTask.java
@@ -0,0 +1,7 @@
+package utils;
+
+/**
+ * Created by fabrizio on 04/10/15.
+ */
+public class UploadTask {
+}
