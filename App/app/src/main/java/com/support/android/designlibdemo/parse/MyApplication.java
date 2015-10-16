542ca7654a49e7963deef88edcd0f0edc9451640
jasmina.sella <jasmina.sella@fdvsolutions.com>
2015-10-16 01:46:09 -0300

    Parse: clases herramienta para usarlo

---
+++ App/app/src/main/java/com/support/android/designlibdemo/parse/MyApplication.java
@@ -0,0 +1,22 @@
+package com.support.android.designlibdemo.parse;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+
+    private static MyApplication mInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+
+        // register with parse
+        ParseUtils.registerParse(this);
+    }
+
+
+    public static synchronized MyApplication getInstance() {
+        return mInstance;
+    }
+}
