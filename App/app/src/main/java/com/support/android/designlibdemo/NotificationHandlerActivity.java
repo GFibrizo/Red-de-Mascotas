c455327059d95541df3f13c288697ea48327b658
jasmina.sella <jasmina.sella@fdvsolutions.com>
2015-10-19 15:10:01 -0300

    Notificaciones push: pasa de notificacion de adopcion a la pantalla de notificaciones

--- App/app/src/main/java/com/support/android/designlibdemo/NotificationHandlerActivity.java
+++ App/app/src/main/java/com/support/android/designlibdemo/NotificationHandlerActivity.java
@@ -1,7 +1,20 @@
 package com.support.android.designlibdemo;
 
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class NotificationHandlerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_result_list_view);
+
+
+    }
+
 
 }
---
+++ App/app/src/main/java/utils/MatchRequest.java
@@ -0,0 +1,50 @@
+package utils;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.RequestFuture;
+
+import org.json.JSONArray;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+/** Trae los match de un usuario a partir de su Id **/
+
+public class MatchRequest {
+    RequestHandler requestHandler;
+
+    public MatchRequest(Context context) {
+        requestHandler = RequestHandler.getInstance(context);
+        requestHandler.setContext(context);
+    }
+
+    public JSONArray getMatch(String userId) {
+
+        String path =  requestHandler.getServerUrl() + this.buildSearchNotificationPath(userId);
+
+        RequestFuture<JSONArray> future = RequestFuture.newFuture();
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, path, future, future);
+        requestHandler.addToRequestQueue(request);
+        JSONArray response = null;
+        try {
+            response =  future.get(10, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            Log.e("Retrieve cards api call interrupted.", String.valueOf(e));
+        } catch (ExecutionException e) {
+            Log.e("Retrieve cards api call failed.", String.valueOf(e));
+        } catch (TimeoutException e) {
+            Log.e("Retrieve cards api call timed out.", String.valueOf(e));
+        }
+        return response;
+    }
+
+    private String buildSearchNotificationPath(String userId) {
+        return "/user/"+userId+"/pets/matches";
+    }
+
+}
