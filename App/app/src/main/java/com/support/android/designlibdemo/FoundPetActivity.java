94a4d0a78923b85db63dab96cf0723acf071c5e6
jasmina.sella <jasmina.sella@fdvsolutions.com>
2015-10-29 00:55:02 -0300

    Cambios del lunes en FoundPetActivity: Mensaje de error de conexion

--- App/app/src/main/java/com/support/android/designlibdemo/FoundPetActivity.java
+++ App/app/src/main/java/com/support/android/designlibdemo/FoundPetActivity.java
@@ -655,6 +655,7 @@ public class FoundPetActivity extends AppCompatActivity implements
                 response = request.post(object);
             } catch (InterruptedException | ExecutionException  | TimeoutException e) {
                 // exception handling
+                Log.e("Found Error",e.getMessage());
                 return false;
             }
             return true;
@@ -662,13 +663,14 @@ public class FoundPetActivity extends AppCompatActivity implements
 
         @Override
         protected void onPostExecute(final Boolean success) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             if (success) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 Toast.makeText(getApplicationContext(), "Exito", Toast.LENGTH_SHORT).show();
-                startActivity(intent);
+
             }else{
-                Toast.makeText(getApplicationContext(), "Error de conexion", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(getApplicationContext(), "Error de conexion", Toast.LENGTH_SHORT).show();
             }
+            startActivity(intent);
         }
 
         @Override
